bfb199b889c93912b4818672d102e95512224a7a
John McLear <john@mclear.co.uk>
2013-11-07 23:06:44 +0000

    styling

--- static/js/main.js
+++ static/js/main.js
@@ -6,6 +6,6 @@ exports.postAceInit = function(hook_name, args, cb) {
   var classes = clientVars.ep_button_link.classes;
   var button = "<li><a class='"+classes+"' target='_blank' href='"+link+"'><span id='ep_button_link_span' class='buttonicon'>" + text + "</span></a></li>";
   $(before).parent().before(button);
-  $('#ep_button_link_span').css({"background-image":"none", "width": "auto", "color":"#666"});
+  $('#ep_button_link_span').css({"background-image":"none", "width": "auto", "color":"#666", "font-size":"16px", "margin-top":"-3px"});
  
 }
