2ff07c97e2f07076aeefcc13c9b26c0c4afc02d6
Richard Hansen <rhansen@rhansen.org>
2021-08-25 16:47:50 -0400

    Process `.before` and `.after` together

--- index.js
+++ index.js
@@ -15,16 +15,15 @@ exports.clientVars = (hook, context, callback) => {
     logger.warn('Missing text; set ep_button_link.text in settings.json.');
     ep_button_link.text = 'NO TEXT SET';
   }
-  if (!ep_button_link.before) {
-    logger.info('Missing before; add ep_button_link.before to settings.json.');
+  if (ep_button_link.after && ep_button_link.before) {
+    logger.warn('Ignoring ep_button_link.before because .after is set in settings.json.');
+    delete ep_button_link.before;
+  } else if (!ep_button_link.after && !ep_button_link.before) {
     ep_button_link.before = "[data-key='showTimeSlider']";
   }
   if (!ep_button_link.classes) {
     logger.info('Missing classes; set ep_button_link.classes in settings.json.');
     ep_button_link.classes = 'grouped-left';
-  }
-  if (!ep_button_link.after) {
-    logger.info('Missing after; set ep_button_link.after in settings.json.');
   }
   return callback({ep_button_link});
 };
