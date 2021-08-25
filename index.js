13a6b119c14bbf0d9878e70340a3f2caa68b2e1a
Richard Hansen <rhansen@rhansen.org>
2021-08-25 17:57:38 -0400

    Process settings in `loadSettings` hook

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 'use strict';
 
 const log4js = require('ep_etherpad-lite/node_modules/log4js');
-const settings = require('ep_etherpad-lite/node/utils/Settings');
 
 const logger = log4js.getLogger('ep_button_link');
+let settings = null;
 
-exports.clientVars = async (hookName, context) => {
-  const {ep_button_link = {}} = settings;
-  if (!ep_button_link.link) {
+exports.clientVars = async (hookName, context) => ({ep_button_link: settings});
+
+exports.loadSettings = async (hookName, {settings: {ep_button_link}}) => {
+  settings = ep_button_link;
+  if (!settings) {
+    logger.warn('Missing settings in settings.json; no button will be displayed.');
+    settings = null;
+    return;
+  }
+  if (!settings.link) {
     logger.warn('Missing ep_button_link.link in settings.json; no button will be displayed.');
-    return {};
+    settings = null;
+    return;
   }
-  if (ep_button_link.after && ep_button_link.before) {
+  if (settings.before && settings.after) {
     logger.warn('Ignoring ep_button_link.before because .after is set in settings.json.');
-    delete ep_button_link.before;
-  } else if (!ep_button_link.after && !ep_button_link.before) {
-    ep_button_link.before = "[data-key='showTimeSlider']";
+    delete settings.before;
+  } else if (!settings.before && !settings.after) {
+    settings.before = '[data-key="showTimeSlider"]';
   }
-  if (!ep_button_link.classes) ep_button_link.classes = 'grouped-left';
-  return {ep_button_link};
+  if (!settings.classes) settings.classes = 'grouped-left';
 };
