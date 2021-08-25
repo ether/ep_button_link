2cb6f07c1a5e7de4746db0b30f5ef1974a1629ac
Richard Hansen <rhansen@rhansen.org>
2021-08-25 16:43:27 -0400

    Use the settings object directly for clientVars
    This simplifies the code, and it avoids repetetive warnings (assuming
    `settings.ep_button_link` is set).

--- index.js
+++ index.js
@@ -6,46 +6,25 @@ const settings = require('ep_etherpad-lite/node/utils/Settings');
 const logger = log4js.getLogger('ep_button_link');
 
 exports.clientVars = (hook, context, callback) => {
-  let ep_button_link = {};
-  if (settings.ep_button_link) {
-    if (!settings.ep_button_link.link) {
-      logger.warn('Missing link; set ep_button_link.link in settings.json.');
-      ep_button_link.link = 'https://github.com/JohnMcLear/ep_button_link';
-    } else {
-      ep_button_link.link = settings.ep_button_link.link;
-    }
-    if (!settings.ep_button_link.text) {
-      logger.warn('Missing text; set ep_button_link.text in settings.json.');
-      ep_button_link.text = 'NO TEXT SET';
-    } else {
-      ep_button_link.text = settings.ep_button_link.text;
-    }
-    if (!settings.ep_button_link.before) {
-      logger.info('Missing before; add ep_button_link.before to settings.json.');
-      ep_button_link.before = "[data-key='showTimeSlider']";
-    } else {
-      ep_button_link.before = settings.ep_button_link.before;
-    }
-    if (!settings.ep_button_link.classes) {
-      logger.info('Missing classes; set ep_button_link.classes in settings.json.');
-      ep_button_link.classes = 'grouped-left';
-    } else {
-      ep_button_link.classes = settings.ep_button_link.classes;
-    }
-    if (!settings.ep_button_link.after) {
-      logger.info('Missing after; set ep_button_link.after in settings.json.');
-    } else {
-      ep_button_link.after = settings.ep_button_link.after;
-    }
-  } else {
-    logger.warn(
-        'Missing settings in settings.json; example: ' +
-        '"ep_button_link": {"link": "https://github.com/JohnMcLear/ep_button", "text": "TEST"}');
-    ep_button_link = {};
+  const {ep_button_link = {}} = settings;
+  if (!ep_button_link.link) {
+    logger.warn('Missing link; set ep_button_link.link in settings.json.');
     ep_button_link.link = 'https://github.com/JohnMcLear/ep_button_link';
+  }
+  if (!ep_button_link.text) {
+    logger.warn('Missing text; set ep_button_link.text in settings.json.');
     ep_button_link.text = 'NO TEXT SET';
-    ep_button_link.before = '.buttonicon-history';
-    ep_button_link.classes = 'grouped-right';
+  }
+  if (!ep_button_link.before) {
+    logger.info('Missing before; add ep_button_link.before to settings.json.');
+    ep_button_link.before = "[data-key='showTimeSlider']";
+  }
+  if (!ep_button_link.classes) {
+    logger.info('Missing classes; set ep_button_link.classes in settings.json.');
+    ep_button_link.classes = 'grouped-left';
+  }
+  if (!ep_button_link.after) {
+    logger.info('Missing after; set ep_button_link.after in settings.json.');
   }
   return callback({ep_button_link});
 };
