76fcef5e069094562a9a05a00881b68cc3f2475c
Richard Hansen <rhansen@rhansen.org>
2021-08-25 16:29:11 -0400

    Use log4js logger; tweak wording

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
 'use strict';
 
+const log4js = require('ep_etherpad-lite/node_modules/log4js');
 const settings = require('ep_etherpad-lite/node/utils/Settings');
+
+const logger = log4js.getLogger('ep_button_link');
 
 exports.clientVars = (hook, context, callback) => {
   let ep_button_link = {};
   try {
     if (settings.ep_button_link) {
       if (!settings.ep_button_link.link) {
-        console.warn('No link set for ep_button_link, add ep_button_link.link to settings.json');
+        logger.warn('Missing link; set ep_button_link.link in settings.json.');
         ep_button_link.link = 'https://github.com/JohnMcLear/ep_button_link';
       } else {
         ep_button_link.link = settings.ep_button_link.link;
       }
       if (!settings.ep_button_link.text) {
+        logger.warn('Missing text; set ep_button_link.text in settings.json.');
         ep_button_link.text = 'NO TEXT SET';
-        console.warn('No text set for ep_button_link, add ep_button_link.text to settings.json');
       } else {
         ep_button_link.text = settings.ep_button_link.text;
       }
       if (!settings.ep_button_link.before) {
+        logger.info('Missing before; add ep_button_link.before to settings.json.');
         ep_button_link.before = "[data-key='showTimeSlider']";
-        console.info('No before set for ep_button_link, this may be intentional, ' +
-          'add ep_button_link.before to settings.json');
       } else {
         ep_button_link.before = settings.ep_button_link.before;
       }
       if (!settings.ep_button_link.classes) {
+        logger.info('Missing classes; set ep_button_link.classes in settings.json.');
         ep_button_link.classes = 'grouped-left';
-        console.info('No classes set for ep_button_link, this may be intentional, ' +
-        'add ep_button_link.classes to settings.json');
       } else {
         ep_button_link.classes = settings.ep_button_link.classes;
       }
       if (!settings.ep_button_link.after) {
-        console.info('No after set for ep_button_link, this may be intentional,' +
-        ' add ep_button_link.classes to settings.json');
+        logger.info('Missing after; set ep_button_link.after in settings.json.');
       } else {
         ep_button_link.after = settings.ep_button_link.after;
       }
     } else {
-      console.warn('Set ep_button_link settings in settings.json, example is: "ep_button_link":{ "link" : "https://github.com/JohnMcLear/ep_button", "text": "TEST"}');
+      logger.warn(
+          'Missing settings in settings.json; example: ' +
+          '"ep_button_link": {"link": "https://github.com/JohnMcLear/ep_button", "text": "TEST"}');
       ep_button_link = {};
       ep_button_link.link = 'https://github.com/JohnMcLear/ep_button_link';
       ep_button_link.text = 'NO TEXT SET';
       ep_button_link.before = '.buttonicon-history';
       ep_button_link.classes = 'grouped-right';
-      console.warn('No link set for ep_button_link, add ep_button_link.link to settings.json');
-      console.warn('No text set for ep_button_link, add ep_button_link.text to settings.json');
     }
   } catch (e) {
     // lovely try catch me if you can
